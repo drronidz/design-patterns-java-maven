4341ecb5600c84b1442e3fbcf8b9b424b7201220
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-08-08 16:50:39 +0100

    2. Creational Design Patterns  3. Singleton   3. Double Checked Locking Approach (Course Example)   - Implementation

---
+++ 2.Creational Design Patterns/3.Singleton/3.Double Checked Locking Approach/src/main/java/com/course/Client.java
@@ -0,0 +1,14 @@
+package com.course;
+
+/*
+PROJECT NAME : design-patterns-java
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 7/26/2021 3:25 PM
+*/
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
