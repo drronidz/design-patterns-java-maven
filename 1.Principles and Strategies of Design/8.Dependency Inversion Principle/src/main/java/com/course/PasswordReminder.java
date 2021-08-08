77fa4d338aeccc28df350892633a7b5ca638bd50
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-08-08 14:44:22 +0100

    1. Principles and Strategies of Design  8. The Dependency Inversion Principle  -(Course Example)  -Implementation.

---
+++ 1.Principles and Strategies of Design/8.Dependency Inversion Principle/src/main/java/com/course/PasswordReminder.java
@@ -0,0 +1,16 @@
+package com.course;
+
+/*
+PROJECT NAME : design-patterns-java
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 7/21/2021 2:27 AM
+*/
+
+public class PasswordReminder {
+    private DBConnection dbConnection;
+
+    public PasswordReminder(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+}
