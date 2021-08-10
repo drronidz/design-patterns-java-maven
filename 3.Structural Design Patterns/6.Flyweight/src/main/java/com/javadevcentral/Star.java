898ef2b80ec330fb35e13f5dfeb782f4619656a7
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-08-10 17:00:28 +0100

    3. Structural Design Patterns  6. Flyweight (javadevcentral Example - Enhanced Version)   - Implementation

--- 3.Structural Design Patterns/6.Flyweight/src/main/java/com/javadevcentral/Star.java
+++ 3.Structural Design Patterns/6.Flyweight/src/main/java/com/javadevcentral/Star.java
@@ -4,9 +4,25 @@ package com.javadevcentral;
 PROJECT NAME : design-patterns-java-maven
 Module NAME: IntelliJ IDEA
 Author Name : @ DRRONIDZ
-DATE : 8/10/2021 3:39 PM
+DATE : 8/10/2021 3:33 PM
 */
 
-public interface Star {
-    void extractGem(Location location);
+
+
+public class Star {
+    private String name;
+    private Color color;
+    private long weight;
+    private long mass;
+    private long birghtness;
+    private long gravitationalForce;
+
+    // The location
+    private int x;
+    private int y;
+
+    public void extractGem() {
+        System.out.println("Extracting the gems of star " + name + " of color "
+                + color + " at location (" + x +", " + y + ")");
+    }
 }
