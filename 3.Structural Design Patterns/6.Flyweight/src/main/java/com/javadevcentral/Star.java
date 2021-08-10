0e131cf704abf642ffb37b40fa80d455a9ab957f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-08-10 17:01:55 +0100

    Revert "3. Structural Design Patterns  6. Flyweight (javadevcentral Example - Enhanced Version)   - Implementation"
    This reverts commit 898ef2b8

--- 3.Structural Design Patterns/6.Flyweight/src/main/java/com/javadevcentral/Star.java
+++ 3.Structural Design Patterns/6.Flyweight/src/main/java/com/javadevcentral/Star.java
@@ -4,25 +4,9 @@ package com.javadevcentral;
 PROJECT NAME : design-patterns-java-maven
 Module NAME: IntelliJ IDEA
 Author Name : @ DRRONIDZ
-DATE : 8/10/2021 3:33 PM
+DATE : 8/10/2021 3:39 PM
 */
 
-
-
-public class Star {
-    private String name;
-    private Color color;
-    private long weight;
-    private long mass;
-    private long birghtness;
-    private long gravitationalForce;
-
-    // The location
-    private int x;
-    private int y;
-
-    public void extractGem() {
-        System.out.println("Extracting the gems of star " + name + " of color "
-                + color + " at location (" + x +", " + y + ")");
-    }
+public interface Star {
+    void extractGem(Location location);
 }
